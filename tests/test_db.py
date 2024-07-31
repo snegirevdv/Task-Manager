fc49f3fee3a6a3998f2c78baddc358b5b1c15c1f
Denis <snegirev.dv@gmail.com>
2024-07-31 15:44:50 +0300

    Add db test

---
+++ tests/test_db.py
@@ -0,0 +1,18 @@
+from django.db import connection
+
+
+def test_db_table_creation_success():
+    tables = [
+        'labels_tasklabel',
+        'statuses_taskstatus',
+        'tasks_task',
+        'tasks_tasklabelrelation',
+        'users_user',
+    ]
+
+    with connection.cursor() as cursor:
+        for table_name in tables:
+            try:
+                cursor.execute(f"SELECT * FROM {table_name};")
+            except Exception as e:
+                assert False, str(e)
