e6bdec487e6e496bd8177c2f4e6e77d16a8fddb2
Denis <snegirev.dv@gmail.com>
2024-08-10 21:18:58 +0300

    Fix logging

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -140,7 +140,7 @@ LOGGING = {
         "file": {
             "level": "WARNING",
             "class": "logging.FileHandler",
-            "filename": BASE_DIR / "logs" / "django.log",
+            "filename": BASE_DIR / "django_log.log",
             "formatter": "format",
         },
     },
